0e25408a3f83ccda260fd989a043712da7ff8fb5
Marc Valeros <marcnathanielvaleros@gmail.com>
2022-12-20 18:20:17 +0800

    fix login with google

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Marc\AppData\Local\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Marc\Desktop\3RD YEAR FILES\CS 2203 Mobile Development\MidtermProject\midterm_project"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Marc\Documents\GitHub\MobDev_Flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
